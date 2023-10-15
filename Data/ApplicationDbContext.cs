e47bd4c8212d3352e0faad09a7f82c629decb4c1
DasuGanesh <ganeshdasu345@gmail.com>
2023-10-15 17:03:10 +0530

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCTASK.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MVCTASK.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+"# MyWebPortal" 
+"# MyWebPortal" 
